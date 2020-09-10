a97cfacf5093be016cd32f6958812fd241cf5f86
eunchan <eunchan0492@naver.com>
2020-09-11 00:27:45 +0900

    web camera

--- Prevent cheating programm.py
+++ Prevent cheating programm.py
@@ -3,54 +3,53 @@ import pyautogui
 import keyboard
 import mouse
 import win32api
+import cv2
 from threading import *
 
-class MyFrame(wx.Frame):
-    def __init__(self, parent, id,title):
-        #Frame 생성
-        wx.Frame.__init__(self, parent,id,title,size=(300,170),pos=wx.DefaultPosition,
-            style = wx.CAPTION | wx.STAY_ON_TOP)
-        #panel 생성
-        panel=wx.Panel(self,-1)
 
+class ShowCapture(wx.Panel):
+    def __init__(self, parent, capture, fps=15):
 
-        #text 추가
-        wx.StaticText(panel, label="웹캠이 녹화중입니다.", pos=(70, 30), style=wx.ALIGN_CENTER)
-        #버튼 추가
-        self.btnEnd = wx.Button(panel, wx.ID_ANY, pos=(95, 70), label='종료')
-        #버튼 이벤트 추가
-        self.Bind(wx.EVT_BUTTON, self.actEnd, self.btnEnd)
-        #마우스 범위 제한-버그있음
-        self.Bind(wx.EVT_ACTIVATE, self.actCursor)
-
-
-    def actCursor(self, evt):
-        print("y")
-        width = win32api.GetSystemMetrics(0)
-        height = win32api.GetSystemMetrics(1)
-        win32api.ClipCursor((0, 20, width, height))
-
-    def actEnd(self, evt):
-        self.Destroy()
-
-
-class MyApp(wx.App):
-    def OnInit(self):
-
-        #전체화면
+        #wx.Frame.__init__(self, parent, id, style=wx.CAPTION | wx.STAY_ON_TOP)
+        #wx.Panel.__init__(self, -1)
         pyautogui.press('f11')
+        #keyboard.block_key('f11')
 
-        #키제한 - 작업관리자 창 따로 해줘야함
-        keyboard.block_key('Alt')
-        keyboard.block_key('f11')
+        wx.Panel.__init__(self, parent)
 
-        wx.InitAllImageHandlers()
-        self.frame = MyFrame(None,-1,'Anti-Fraud Program')
-        self.frame.Show()
-        return True
+        self.SetWindowStyle(wx.NO_BORDER | wx.STAY_ON_TOP)
+        self.capture = capture
+        ret, frame = self.capture.read()
 
-if __name__ == '__main__':
-    app=MyApp(0)
-    app.MainLoop()
+        height, width = frame.shape[:2]
+        parent.SetSize((width, height))
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        self.bmp = wx.BitmapFromBuffer(width, height, frame)
+
+        self.timer = wx.Timer(self)
+        self.timer.Start(1000./fps)
+
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_TIMER, self.NextFrame)
+       
+
+    def OnPaint(self, evt):
+        dc = wx.BufferedPaintDC(self)
+        dc.DrawBitmap(self.bmp, 0, 0)
+
+    def NextFrame(self, event):
+        ret, frame = self.capture.read()
+        if ret:
+            frame = cv2.flip(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), 1)
+            self.bmp.CopyFromBuffer(frame)
+            self.Refresh()
 
 
+capture = cv2.VideoCapture(0)
+
+app = wx.App()
+frame = wx.Frame(None)
+cap = ShowCapture(frame, capture)
+frame.Show()
+app.MainLoop()
